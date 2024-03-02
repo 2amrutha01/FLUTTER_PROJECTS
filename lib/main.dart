cc6dcda5d174c8fa14e8675dc4c5a2baf1c0dc52
Amrutha P A <2amrutha01@gmail.com>
2024-03-02 13:26:29 +0530

    made some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:sample_design/home.dart';
-import 'package:sample_design/print.dart';
-import 'package:sample_design/printing.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,10 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Demo App",
+        title: "HealthCare App",
         routes: {
           '/home': (context) => home(),
-          '/print': (context) => printer(),
         },
         initialRoute: '/',
         home: home());
